e6e2899eac030401bb596f400dff7ce8726833c0
dikaaayy <andikayudhistira3@gmail.com>
2022-05-18 21:26:59 +0700

    making dynamic route for posts

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
-import { prisma } from '../lib/prisma'
+import { prisma } from '../../lib/prisma'
 import Head from 'next/head'
+import { post } from '@prisma/client'
 
 import {
   SiNextdotjs,
@@ -10,6 +11,7 @@ import {
   SiThemoviedatabase,
   SiSocketdotio,
 } from 'react-icons/si'
+import Link from 'next/link'
 
 export async function getStaticProps() {
   const data = await prisma.post.findMany({
@@ -17,6 +19,7 @@ export async function getStaticProps() {
       project_name: true,
       image: true,
       tech_stack: true,
+      // project_title: true,
     },
   })
 
@@ -28,7 +31,7 @@ export async function getStaticProps() {
 }
 
 export default function portfolio({ posts }: any) {
-  console.log(posts)
+  // console.log(posts)
   const filterImage = (images: String) => {
     const array = images.split(',')
     return array[2]
@@ -63,37 +66,33 @@ export default function portfolio({ posts }: any) {
       <div className="mt-20 grid grid-cols-1 gap-y-14 gap-x-4 md:grid-cols-2 md:justify-items-center ">
         {posts.map((post: any) => {
           return (
-            <div
-              className="flex h-[30vh] w-full flex-col justify-start gap-y-1 rounded-lg bg-gray-300 px-5 shadow-lg transition duration-200 hover:scale-[1.015] md:h-[40vh] md:w-[90%] lg:h-[50vh] lg:w-[80%]"
-              key={post.project_name}
-              onClick={() => {
-                console.log('test')
-              }}
-            >
-              <div className="relative flex h-[88%] w-full">
-                <Image
-                  src={filterImage(post.image)}
-                  layout="fill"
-                  objectFit="contain"
-                  priority
-                />
-              </div>
-              <div className="space-y-1 pb-2">
-                <div className="flex items-center gap-x-3">
-                  {techStackArr(post.tech_stack).map((tech) => {
-                    return (
-                      <div key={tech} className="group relative">
-                        <div className="absolute -top-7 -left-5 z-50 flex h-5 min-w-max scale-0 items-center justify-center rounded bg-[#424242] py-3 px-2 text-white transition-all group-hover:scale-100">
-                          <p className="text-xs font-medium">{tech}</p>
-                        </div>
-                        {filterTechStack(tech)}
-                      </div>
-                    )
-                  })}
+            <Link key={post.project_name} href={'https://google.com'}>
+              <div className="flex h-[30vh] w-full flex-col justify-start gap-y-1 rounded-lg bg-gray-300 px-5 shadow-lg transition duration-200 hover:scale-[1.015] md:h-[40vh] md:w-[90%] lg:h-[50vh] lg:w-[80%]">
+                <div className="relative flex h-[88%] w-full">
+                  <Image
+                    src={filterImage(post.image)}
+                    layout="fill"
+                    objectFit="contain"
+                    priority
+                  />
                 </div>
-                <p className="text-xl font-semibold">{post.project_name}</p>
+                <div className="space-y-1 pb-2">
+                  <div className="flex items-center gap-x-3">
+                    {techStackArr(post.tech_stack).map((tech) => {
+                      return (
+                        <div key={tech} className="group relative">
+                          <div className="absolute -top-7 -left-5 z-50 flex h-5 min-w-max scale-0 items-center justify-center rounded bg-[#424242] py-3 px-2 text-white transition-all group-hover:scale-100">
+                            <p className="text-xs font-medium">{tech}</p>
+                          </div>
+                          {filterTechStack(tech)}
+                        </div>
+                      )
+                    })}
+                  </div>
+                  <p className="text-xl font-semibold">{post.project_name}</p>
+                </div>
               </div>
-            </div>
+            </Link>
           )
         })}
       </div>
