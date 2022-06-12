132bf2f89fae322ab4b3f90649912b8fec8f7401
dikaaayy <andikayudhistira3@gmail.com>
2022-06-12 22:06:27 +0700

    styling to comment section

--- pages/portfolio/[project_title].tsx
+++ pages/portfolio/[project_title].tsx
@@ -42,7 +42,7 @@ export default function Detail({ post }: any) {
       <Navbar />
       <>
         <div className="bg-customLight-lightGray pb-10 pt-32 font-sanFrancisco dark:bg-customDark-darkGray">
-          <div className="mx-auto w-[85vw] space-y-3 overflow-hidden rounded-md bg-[#d1c8a6] text-white shadow-xl dark:bg-[#808080] dark:text-black sm:w-[80vw] lg:w-[70vw] xl:w-[65vw]">
+          <div className="mx-auto w-[85%] space-y-3 overflow-hidden rounded-md bg-[#d1c8a6] text-white shadow-xl dark:bg-[#808080] dark:text-black sm:w-[80vw] lg:w-[70vw] xl:w-[65vw]">
             <div className="px-5 pt-3">
               {isMounted ? (
                 <Image src={images[0]} width={2532} height={1472} />
@@ -85,7 +85,24 @@ export default function Detail({ post }: any) {
               <p className="select-none tracking-wider">{post.content}</p>
             </div>
           </div>
-          <p>Test</p>
+          <div className="mx-auto mt-10 w-[85%] rounded-md border-2 bg-[#d1c8a6] p-5 text-white shadow-xl dark:bg-[#d6d6d6] dark:text-black sm:w-[80vw] lg:w-[70vw] xl:w-[65%]">
+            <p className="text-lg font-medium">Leave Your Comment below!</p>
+            <form className="mt-5 flex flex-col">
+              <label htmlFor="comment">Write here</label>
+              <textarea
+                id="comment"
+                className="resize rounded bg-white p-2 outline-none"
+                maxLength={400}
+                spellCheck={false}
+              />
+              <button
+                type="submit"
+                className="mt-2 w-1/5 self-end rounded-md border-2 py-2"
+              >
+                Post
+              </button>
+            </form>
+          </div>
         </div>
       </>
     </>
