eebe3b2b541112ca0e33ec0853919f324baf4acd
dikaaayy <andikayudhistira3@gmail.com>
2022-07-22 17:36:03 +0700

    auth fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,12 +10,12 @@ export default (req: NextApiRequest, res: NextApiResponse) =>
     // Configure one or more authentication providers
     providers: [
       GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
+        clientId: process.env.GITHUB_ID!,
+        clientSecret: process.env.GITHUB_SECRET!,
       }),
       GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       }),
       // ...add more providers here
     ],
